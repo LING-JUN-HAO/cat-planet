618c4a4134f786d8d39910e54290a5cf0df3b80a
Antonio <antonio@AntoniodeMacBook-Air.local>
2024-03-09 15:09:03 +0800

    fix: 🐛 replace empty cat img

--- src/views/consumer/CartPage.vue
+++ src/views/consumer/CartPage.vue
@@ -69,7 +69,7 @@
   <section v-if="cart.carts.length === 0 && defaultStatus == true" class="container-title">
     <h2 data-aos="fade-down" data-aos-delay="0" data-aos-duration="900" class="text-center py-3 fw-bold">當前購物車無商品</h2>
     <div data-aos="fade-up" data-aos-delay="450" data-aos-duration="900" class="py-5 col-3 m-auto empty-img-box">
-      <img src="../../assets/image/empty.png" class="object-fit-cover w-100 empty-img" alt="空購物車">
+      <img src="../../assets/image/empty2.png" class="object-fit-cover w-100 empty-img" alt="空購物車">
     </div>
   </section>
   <div v-if="defaultStatus == true" class="pt-3 pb-4 text-center">
