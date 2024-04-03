ae5b66ae099d5c9b4c7aae5dae82262b65c3b896
Antonio <antonio@AntoniodeMacBook-Air.local>
2024-04-03 22:54:05 +0800

    fix: 🐛 pink color

--- src/components/utils/ScrollTop.vue
+++ src/components/utils/ScrollTop.vue
@@ -1,7 +1,30 @@
 <template>
-    <div>
-        <i class="bi bi-arrow-left-short"></i>
-        手機版面時，表格可左右滑動
-        <i class="bi bi-arrow-right-short"></i>
-    </div>
+  <a href="#" class="scrollTopContainer position-fixed bg-primary rounded-3 opacity-75 text-center d-flex align-items-center justify-content-center"
+    :class="scrollYValue < 200 ? 'd-none' : ''" @click.prevent="scrollTop">
+    <i class="bi bi-arrow-up-circle text-white fs-1 fw-bold"></i>
+  </a>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      scrollYValue: ''
+    }
+  },
+  methods: {
+    scrollTop () {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    },
+    scrollWatch () {
+      this.scrollYValue = window.scrollY
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.scrollWatch)
+  }
+}
+</script>
