f1e426f081d3c189fc3fb2e2c36c8815a36e9ea8
Antonio <antonio@AntoniodeMacBook-Air.local>
2024-04-11 00:27:31 +0800

    style: 💄 homePage text style change

--- src/components/consumer/FooterComponent.vue
+++ src/components/consumer/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="mt-auto footerContainer object-fit-cover position-relative">
     <div class="footerContent position-absolute top-50 translate-middle-y me-5">
-      <h1 class="fs-2 fw-bold mb-3">訂閱獲取貓咪相關資訊</h1>
+      <h1 class="fs-2 fw-bold mb-3">訂閱獲取貓咪優惠資訊</h1>
       <VForm ref="creditCardForm" class="col-md-12" v-slot="{ errors }" @submit="emailSend">
         <div class="d-flex">
           <VField id="email" name="email" type="email" class="form-control text-truncate me-2"
--- src/components/consumer/homePage/CategoryComponent.vue
+++ src/components/consumer/homePage/CategoryComponent.vue
@@ -1,34 +1,34 @@
 <template>
   <section class="category container container-title">
-    <h2 data-aos="fade-down" data-aos-delay="0" data-aos-duration="900" class="text-center py-6 m-0 fw-bold">商品分類</h2>
+    <h2 data-aos="fade-down" data-aos-delay="0" data-aos-duration="900" class="text-center py-6 m-0 fw-bold">貓咪寶藏</h2>
     <ul data-aos="fade-up" data-aos-delay="450" data-aos-duration="900"
       class="content-shadow border border-1 bg-white rounded-4 p-4 p-md-6 row m-0">
       <li class="category-item col-xs-12 col-sm-6 col-md-6 col-lg-3 py-3 py-md-0">
         <router-link :to="{ path: '/consumer/products', query: { category: '貓咪玩具', page: 1 } }"
           class="text-decoration-none  d-flex justify-content-center align-items-center flex-column">
           <img src="@/assets/image/category-toys.jpeg" class="rounded-4 p-2"  alt="貓咪玩具">
-          <h3 class="fw-normal lh-lg text-black text-center">貓咪玩具</h3>
+          <h3 class="fw-normal lh-lg text-black text-center">貓爪樂園</h3>
         </router-link>
       </li>
       <li class="category-item col-xs-12 col-sm-6 col-md-6 col-lg-3 pb-3 pb-md-0">
         <router-link :to="{ path: '/consumer/products', query: { category: '美容護理', page: 1 } }"
           class="text-decoration-none  d-flex justify-content-center align-items-center flex-column">
           <img src="@/assets/image/category-care.jpeg" class="rounded-4 p-2" alt="美容護理">
-          <h3 class="fw-normal lh-lg text-black text-center">美容護理</h3>
+          <h3 class="fw-normal lh-lg text-black text-center">貓咪SPA</h3>
         </router-link>
       </li>
       <li class="category-item col-xs-12 col-sm-6 col-md-6 col-lg-3 pb-3 pb-md-0">
         <router-link :to="{ path: '/consumer/products', query: { category: '飲食用品', page: 1 } }"
           class="text-decoration-none  d-flex justify-content-center align-items-center flex-column">
           <img src="@/assets/image/category-restaurant.jpeg" class="rounded-4 p-2" alt="飲食用品">
-          <h3 class="fw-normal lh-lg text-black text-center">飲食用品</h3>
+          <h3 class="fw-normal lh-lg text-black text-center">美味餐時</h3>
         </router-link>
       </li>
       <li class="category-item col-xs-12 col-sm-6 col-md-6 col-lg-3 pb-3 pb-md-0">
         <router-link :to="{ path: '/consumer/products', query: { category: '貓咪床窩', page: 1 } }"
           class="text-decoration-none  d-flex justify-content-center align-items-center flex-column">
           <img src="@/assets/image/category-chair.jpeg" class="rounded-4 p-2" alt="貓咪床窩">
-          <h3 class="fw-normal lh-lg text-black text-center">貓咪床窩</h3>
+          <h3 class="fw-normal lh-lg text-black text-center">夢幻貓窩</h3>
         </router-link>
       </li>
     </ul>
